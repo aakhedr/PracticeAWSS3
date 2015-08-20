c6127ab3c7f69d3c6fe97e25bafacfd4d6824577
Ahmed Khedr <ahmed.a.khedr@outlook.com>
2015-08-20 08:32:34 +0200

    Initial Commit

---
+++ PracticeAWS2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PracticeAWS2
+//
+//  Created by Ahmed Khedr on 8/20/15.
+//  Copyright (c) 2015 Ahmed Khedr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
